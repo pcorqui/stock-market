3fda70c33dda6e17362b7063342da05757ed1512
Paul Cortes <pcorqui@gmail.com>
2024-04-01 00:01:22 -0600

    ADD: Stock Class

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -12,6 +12,7 @@ export class StockItemComponent implements OnInit {
   public price: number = 0.0;
   public previousPrice: number = 0.0;
   public positiveChange: boolean = true;
+  public favorite: boolean = false;
 
   constructor() { }
 
@@ -22,6 +23,12 @@ export class StockItemComponent implements OnInit {
     this.price = 85;
     this.previousPrice = 80;
     this.positiveChange = this.price >= this.previousPrice;
+    this.favorite = false;
+  }
+
+  toggleFavorite(){
+    console.log('We are toggling the favorite state for this stock');
+    this.favorite = !this.favorite;
   }
 
 }
